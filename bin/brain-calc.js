8bc4eceaa24048a1f51f33d4677162fc330a3a99
Bohdan Kariev <karievbohdan@gmail.com>
2024-12-21 23:59:08 +0200

    Wrote logic for the calculator game

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,21 +1,17 @@
 #!/usr/bin/env node
 
 import readlineSync from "readline-sync";
-import { greeting } from "../src/cli.js";
+import { printWelcome, getRandomNumber, runGame } from "../src/index.js";
 
-console.log("Welcome to the Brain Games!");
-const userName = greeting();
+const userName = printWelcome();
 
 console.log("What is the result of the expression?");
 
 const operationOfNumbers = ["+", "-", "*"];
 
-let rightAnswers = 0;
-for (let i = 1; i <= 3; i += 1) {
-  const [firstNumber, secondNumber] = [
-    Math.floor(Math.random() * 20),
-    Math.floor(Math.random() * 20),
-  ];
+const gameInstructions = () => {
+  const firstNumber = getRandomNumber();
+  const secondNumber = getRandomNumber();
   const randomOperationOfNumbers =
     operationOfNumbers[Math.floor(Math.random() * 3)];
   console.log(
@@ -39,14 +35,12 @@ for (let i = 1; i <= 3; i += 1) {
 
   if (expression === Number(userInput)) {
     console.log("Correct!");
+    return true;
   } else {
     console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${expression}'.
 Let's try again, ${userName}!`);
-    break;
+    return false;
   }
-  rightAnswers += 1;
 }
 
-if (rightAnswers === 3) {
-  console.log(`Congratulations, ${userName}!`);
-}
+runGame(gameInstructions, userName);
