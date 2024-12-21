4070ac88226d276422b53d160d625ac3af00bb4f
Bohdan Kariev <karievbohdan@gmail.com>
2024-12-22 00:03:38 +0200

    Used new logic

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import { greeting } from "../src/cli.js";
+import {printWelcome} from "../src/index.js";
 
-console.log("Welcome to the Brain Games!");
-greeting();
+printWelcome();
