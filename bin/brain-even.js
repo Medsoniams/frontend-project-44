cf825df668f12e5c909a7fafc4ad8a71a336d8b3
Bohdan Kariev <karievbohdan@gmail.com>
2024-12-21 23:58:09 +0200

    Wrote logic for a game to check for parity

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
 import readlineSync from "readline-sync";
-import { greeting } from "../src/cli.js";
+import { printWelcome, getRandomNumber, runGame } from "../src/index.js";
 
-console.log("Welcome to the Brain Games!");
-const userName = greeting();
+const userName = printWelcome();
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 let rightAnswers = 0;
-
-for (let i = 1; i <= 3; i += 1) {
+const gameInstructions = () => {
     const randomNumber = Math.floor(Math.random() * 100);
     const isEven = randomNumber % 2 === 0;
     console.log(`Question: ${randomNumber}`);
@@ -19,14 +17,12 @@ for (let i = 1; i <= 3; i += 1) {
 
     if ((isEven && userInput === "yes") || (!isEven && userInput === "no")) {
         console.log("Correct!");
-        rightAnswers += 1;
+        return true;
     } else {
         console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${isEven ? "yes" : "no"}'.
 Let's try again, ${userName}!`);
-        break;
+        return false;
     }
 }
 
-if (rightAnswers === 3) {
-    console.log(`Congratulations, ${userName}!`);
-}
+runGame(gameInstructions, userName);
