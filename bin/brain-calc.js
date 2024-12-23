16dc4d4ba9753a88ef4d14ed7c982063518cbfcc
Bohdan Kariev <karievbohdan@gmail.com>
2024-12-23 14:28:48 +0200

    Fixing linter errors

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,46 +1,48 @@
 #!/usr/bin/env node
 
-import readlineSync from "readline-sync";
-import { printWelcome, getRandomNumber, runGame } from "../src/index.js";
+import readlineSync from 'readline-sync';
+import { printWelcome, getRandomNumber, runGame } from '../src/index.js';
 
 const userName = printWelcome();
 
-console.log("What is the result of the expression?");
+console.log('What is the result of the expression?');
 
-const operationOfNumbers = ["+", "-", "*"];
+const operationOfNumbers = ['+', '-', '*'];
 
 const gameInstructions = () => {
   const firstNumber = getRandomNumber();
   const secondNumber = getRandomNumber();
-  const randomOperationOfNumbers =
-    operationOfNumbers[Math.floor(Math.random() * 3)];
+  const randomOperationOfNumbers = operationOfNumbers[Math.floor(Math.random() * 3)];
   console.log(
     `Question: ${firstNumber} ${randomOperationOfNumbers} ${secondNumber}`,
   );
 
   let expression;
   switch (randomOperationOfNumbers) {
-    case "+":
+    case '+':
       expression = firstNumber + secondNumber;
       break;
-    case "-":
+    case '-':
       expression = firstNumber - secondNumber;
       break;
-    case "*":
+    case '*':
       expression = firstNumber * secondNumber;
       break;
+    default:
+      console.error(`Unexpected operator: ${randomOperationOfNumbers}`);
+      return false;
   }
 
-  const userInput = readlineSync.question("Answer: ");
+  const userInput = readlineSync.question('Answer: ');
 
   if (expression === Number(userInput)) {
-    console.log("Correct!");
+    console.log('Correct!');
     return true;
   } else {
     console.log(`'${userInput}' is wrong answer ;(. Correct answer was '${expression}'.
 Let's try again, ${userName}!`);
     return false;
   }
-}
+};
 
 runGame(gameInstructions, userName);
